86b3d0e7ad9259043f18f56feaae9f448aab8f8a
Zach Sherman <zbsherman@MacBook-Pro.local>
2016-11-07 09:59:56 -0500

    Commit 2

--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -3,6 +3,21 @@
 # hashtags #UMSI-206 #Proj3 in the tweet.
 
 # You will demo this live for grading.
+import tweepy
+
+# Unique code from Twitter
+access_token = "44199763-QbzzHMshq9X4iOew1ZIy1ByPWBTmulba6jG5gpWQA"
+access_token_secret = "CHrzNqxCAtuUu7Vk2p9x4Yo9gAQrpEgeMDeFFeWDJdtmL"
+consumer_key = "r4jg39kOcc8cmpjEQANJVL7W7"
+consumer_secret = "UlN5CZoS7LKQojpfxKkA2aRXUjpU2t23gD0I74kFgOEGFgRbS6"
+
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+api = tweepy.API(auth)
+
+api.update_with_media("proj3.jpg", "#UMSI-206 #Proj3")
+
 
 print("""No output necessary although you 
 	can print out a success/failure message if you want to.""")
--- twittercode/twitter.py
+++ twittercode/twitter.py
@@ -9,7 +9,6 @@ consumer_secret = "UlN5CZoS7LKQojpfxKkA2aRXUjpU2t23gD0I74kFgOEGFgRbS6"
 # Boilerplate code here
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token,access_token_secret)
-
 api = tweepy.API(auth)
 #Now we can Create Tweets, Delete Tweets, and Find Twitter Users
 
