6b901e7f9202dfa67e2cc9f75fbdac6777c1203e
Zach Sherman <zbsherman@MacBook-Pro.local>
2016-11-09 13:48:16 -0500

    Fourth commit

--- HW3-StudentCopy/madlibhw3.py
+++ HW3-StudentCopy/madlibhw3.py
@@ -1,5 +1,11 @@
 # Using text2 from the nltk book corpa, create your own version of the
 # MadLib program.  
+import nltk
+from nltk.book import text2
+from nltk import word_tokenize,sent_tokenize
+
+
+
 
 # Requirements:
 # 1) Only use the first 150 tokens
@@ -10,6 +16,25 @@
 # 1) Print the orginal text (150 tokens)
 # 1) Print the new text
 print("START*******")
+tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective"}
+substitution_probabilities = {"NN":.1,"NNS":.2,"VB":.25,"JJ":.25}
 
 
+def spaced(word):
+	if word in [",", ".", "?", "!", ":"]:
+		return word
+	else:
+		return " " + word
+
+final_words = []
+
+
+"""for (word, tag) in tagged_tokens:
+	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
+		final_words.append(spaced(word))
+	else:
+		new_word = input("Please enter %s:\n" % (tagmap[tag]))
+		final_words.append(spaced(new_word))"""
+
+#print ("".join(final_words))
 print("\n\nEND*******")
