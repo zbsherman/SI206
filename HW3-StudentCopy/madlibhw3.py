d8a6e5edd4dc36b6d4942d04779fb207af4aa975
Zach Sherman <zbsherman@MacBook-Pro.local>
2016-11-10 08:53:01 -0500

    Sixth Commit

--- HW3-StudentCopy/madlibhw3.py
+++ HW3-StudentCopy/madlibhw3.py
@@ -1,12 +1,14 @@
 # Using text2 from the nltk book corpa, create your own version of the
 # MadLib program.  
 import nltk
-from nltk.book import text2
+from nltk.book import *
 from nltk import word_tokenize,sent_tokenize
+import random
 
 
-
-
+#print(rd)
+#tokens = nltk.word_tokenize(text2)
+#print(tokens)
 # Requirements:
 # 1) Only use the first 150 tokens
 # 2) Pick 5 parts of speech to prompt for, including nouns
@@ -16,8 +18,11 @@ from nltk import word_tokenize,sent_tokenize
 # 1) Print the orginal text (150 tokens)
 # 1) Print the new text
 print("START*******")
+tokens = text2[11:168]
+tagged_tokens = nltk.pos_tag(tokens)
+
 tagmap = {"NN":"a noun","NNS":"a plural noun","VB":"a verb","JJ":"an adjective"}
-substitution_probabilities = {"NN":.1,"NNS":.2,"VB":.25,"JJ":.25}
+substitution_probabilities = {"NN":.15,"NNS":.1,"VB":.1,"JJ":.1,"PRP":.1}
 
 
 def spaced(word):
@@ -27,14 +32,20 @@ def spaced(word):
 		return " " + word
 
 final_words = []
+old_words = []
 
+for word in tokens:
+	old_words.append(spaced(word))
 
-"""for (word, tag) in tagged_tokens:
+for (word, tag) in tagged_tokens:
 	if tag not in substitution_probabilities or random.random() > substitution_probabilities[tag]:
 		final_words.append(spaced(word))
 	else:
 		new_word = input("Please enter %s:\n" % (tagmap[tag]))
-		final_words.append(spaced(new_word))"""
+		final_words.append(spaced(new_word))
 
-#print ("".join(final_words))
+print("\nOriginal")
+print("".join(old_words))
+print("\nNew")
+print ("".join(final_words))
 print("\n\nEND*******")
